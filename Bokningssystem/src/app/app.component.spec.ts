1d75632faea2f4adf5a438ac2fd3feda634da1f7
Sannicolo <victor@sannicolo.se>
2023-02-24 13:51:13 +0100

    Ny version - test

--- Bokningssystem/src/app/app.component.spec.ts
+++ Bokningssystem/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Bokningssystem2'`, () => {
+  it(`should have as title 'Bokningssystem'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Bokningssystem2');
+    expect(app.title).toEqual('Bokningssystem');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Bokningssystem2 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Bokningssystem app is running!');
   });
 });
--- Bokningssystem/src/app/app.component.ts
+++ Bokningssystem/src/app/app.component.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
+import { doc, setDoc } from "firebase/firestore";
+//import { db } from '../environments/environment';
 
+/* // Add a new document in collection "cities"
+await setDoc(doc(db, "items", "LA"), {
+  name: "Los Angeles",
+  state: "CA",
+  country: "USA"
+}); */
 
 @Component({
   selector: 'app-root',
@@ -10,9 +18,11 @@ import { Observable } from 'rxjs';
 })
 
 export class AppComponent {
+
   title = 'Bokningssystem';
   items: Observable<any[]>;
   constructor(firestore: AngularFirestore) {
     this.items = firestore.collection('items').valueChanges();
   }
 }
+
