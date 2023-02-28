5e5a2d798beea032f7b9d22eb580051f2a470041
kaan sirin <kaansirin@yahoo.com>
2023-02-28 12:37:22 +0100

    enter problematiken is no more

--- Bokningssystem/src/app/search-booking/search-booking.component.spec.ts
+++ Bokningssystem/src/app/search-booking/search-booking.component.spec.ts
@@ -16,7 +16,6 @@ describe('SearchBookingComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
   it('should create', () => {
     expect(component).toBeTruthy();
   });
